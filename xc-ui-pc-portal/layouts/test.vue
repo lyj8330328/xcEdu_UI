89d92b1ba0ba7e7e508fbd8ca2812734cef5fb47
lyj8330328 <980500807@qq.com>
2019-06-05 22:02:34 +0800

    课程搜索Nuxt工程搭建

---
+++ xc-ui-pc-portal/layouts/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      头部
+    </div>
+    <nuxt />
+    <div>
+      尾部
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ xc-ui-pc-portal/pages/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      用户管理
+    </div>
+</template>
+
+<script>
+    export default {
+      layout: 'test'
+    }
+</script>
+
+<style scoped>
+
+</style>
